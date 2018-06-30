260cb73a7f73eacf2ac7bbbfbb65c90f8498680e
unknown <monmaramba@yahoo.com>
2018-06-30 06:56:28 -0700

    more html and css plus readme and js psuedo code

---
+++ assets/javascript/rps.js
@@ -0,0 +1,22 @@
+//initialize firebase
+
+//create array for RPS
+
+//funtion to render RPS buttons
+
+//on click function for player name input, build it as an object with wins, losses and ties. Display the name.
+
+//function to send data to firebase
+
+//function to deal with data from firebase and display
+
+//game mechanics
+
+//on click event for player's turn
+
+//disabling player window when it's not their turn
+
+//chat window
+
+
+
